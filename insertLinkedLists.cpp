f71e78e6f740bcf146ce7a5cdd1ffe9cdc64889f
badugunn <44510722+badugunn@users.noreply.github.com>
2018-10-29 21:40:08 -0700

    Add files via upload
    This is the program for adding a node at the beginning, end and at nth position in a linked list

---
+++ insertLinkedLists.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+struct Node {
+int data;
+struct Node* next;
+};
+void insertatEnd(int data);
+void Print();
+void insertatStart(int data);
+void insertatN(int data, int n);
+struct Node* head;
+
+using namespace std;
+
+int main()
+{
+   head = NULL;
+   insertatEnd(2);
+   insertatStart(3);
+   insertatN(5,1);
+   insertatN(6,2);
+   Print();
+    //cout << "Hello world!" << endl;
+    return 0;
+}
+void insertatEnd(int data)
+{
+   Node* ntemp = new Node();
+   ntemp->data=data;
+   ntemp->next = NULL;
+   if(head == NULL)
+   {
+   head = ntemp;
+   return;
+   }
+   Node* temp = head;
+   while (temp->next!= NULL)
+   {
+      temp = temp->next;
+   }
+
+   temp->next=ntemp;
+
+return;
+}
+void Print()
+{
+   Node* temp = head;
+   cout << "List is "<<endl;
+   while(temp!=NULL)
+   {
+      cout<<temp->data<<endl;
+      temp = temp->next;
+   }
+return;
+}
+void insertatStart(int data)
+{
+   Node* temp = new Node();
+   temp->data = data;
+   temp->next=head;
+   head = temp;
+return;
+}
+void insertatN(int data, int n)
+{
+   Node* temp = new Node();
+   temp->data = data;
+   if(n==1)
+   {
+      temp->next = head;
+      head = temp;
+      return;
+   }
+   else {
+      Node* temp2 = head;
+      for(int i=1; i<n-1; i++)
+      {
+         temp2 = temp2->next;
+      }
+      temp->next = temp2->next;
+      temp2->next= temp;
+      return;
+   }
+return;
+}
