95cb8f60b4d82afc04ba8969681276f6b5d2e985
badugunn <44510722+badugunn@users.noreply.github.com>
2019-02-04 14:29:11 -0600

    Add files via upload

---
+++ graphs.cpp
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<iostream>
+#include<stdlib.h>
+#include<vector>
+using namespace std;
+
+
+
+void addEdge(vector<int> adj[], int u, int v)
+{
+  adj[u].push_back(v);
+  adj[v].push_back(u);
+}
+
+void printGraph(vector<int> adj[], int V)
+{
+  for(int i=0; i<V; i++)
+  {
+    cout<<"Adjacency list of edge "<< i;
+    for(int j=0; j<adj[i].size(); j++)
+      cout<<"->"<<adj[i][j];
+    cout<<endl;
+  }
+}
+
+int main()
+{
+  int V = 5;
+  vector<int> adj[V];
+  addEdge(adj, 0, 1);
+  addEdge(adj, 0, 4);
+  addEdge(adj, 1, 2);
+  addEdge(adj, 1, 3);
+  addEdge(adj, 1, 4);
+  addEdge(adj, 2, 3);
+  addEdge(adj, 3, 4);
+  printGraph(adj, V);
+  return 0;
+}
